9b95e8de9627af3a882272b939a8bea20d9f7087
komed3 <pk97@outlook.de>
2025-10-23 23:22:09 +0200

    define even more util types

--- types/utils.d.ts
+++ types/utils.d.ts
@@ -8,3 +8,21 @@ export type MergeMany< T extends any[] > = T extends [ infer H, ...infer R ] ? M
 
 /** Convert a union to an intersection */
 export type UnionToIntersection< U > = ( U extends any ? ( k: U ) => void : never ) extends ( ( k: infer I ) => void ) ? I : never;
+
+/** Extract keys of T whose values match V */
+export type KeysByValue< T, V > = { [ K in keyof T ]-?: T[ K ] extends V ? K : never }[ keyof T ];
+
+/** Pick by value type */
+export type PickByValue< T, V > = Pick< T, KeysByValue< T, V > >;
+
+/** Omit keys whose value matches V */
+export type OmitByValue< T, V > = Omit< T, KeysByValue< T, V > >;
+
+/** Make optional keys */
+export type OptionalKeys< T > = { [ K in keyof T ]-?: {} extends Pick< T, K > ? K : never }[ keyof T ];
+
+/** Make required keys */
+export type RequiredKeys< T > = { [ K in keyof T ]-?: {} extends Pick< T, K > ? never : K }[ keyof T ];
+
+/** Expand/flatten insertion points for nicer hover results */
+export type Expand< T > = T extends object ? { [ K in keyof T ]: T[ K ] } & {} : T;
