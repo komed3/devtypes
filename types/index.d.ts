52cd637bbcb4d1b3684e53fa75a823c9223bf9aa
komed3 <pk97@outlook.de>
2025-10-24 00:00:01 +0200

    Updaet index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,11 @@
+/**
+ * Modular collection of high-abstraction TypeScript types.
+ * 
+ * @author Paul Köhler (komed3)
+ * @version 1.0.0
+ * @license MIT
+ */
+
 export * from './base';
 export * from './collections';
 export * from './combinators';
