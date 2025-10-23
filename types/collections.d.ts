b25861ea8736efd17eb845a845e3dc3689bda530
komed3 <pk97@outlook.de>
2025-10-23 23:40:11 +0200

    implement path helper types

--- types/collections.d.ts
+++ types/collections.d.ts
@@ -29,3 +29,20 @@ export type DeepMutable< T > = T extends Function ? T
     : T extends Array< infer U > ? Array< DeepMutable< U > >
     : T extends object ? { -readonly [ K in keyof T ]-?: DeepMutable< T[ K ] > }
     : T;
+
+/** Path helpers: nested property path as dot-separated keys (string literal union) */
+export type Join< K, P > = K extends string | number ? P extends string | number
+    ? `${ K & ( string | number ) }.${ P & ( string | number ) }`
+    : never : never;
+
+type Prev = [ never, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+
+/** Build all nested paths for an object up to a reasonable recursion depth */
+export type Paths< T, D extends number = 5 > = [ D ] extends [ never ] ? never : T extends object ? {
+    [ K in keyof T ]-?: K extends string | number ? T[ K ] extends object ? K | Join< K, Paths< T[ K ], Prev[ D ] > > : K : never
+}[ keyof T ] : '';
+
+/** Get a nested value by path (dot notation) */
+export type PathValue< T, P extends string > = P extends `${ infer K }.${ infer Rest }`
+    ? K extends keyof T ? PathValue< T[ K ], Rest > : never
+    : P extends keyof T ? T[ P ] : never;
