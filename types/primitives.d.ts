53f0fc1d0879f54f8bcc7cac1f81a36360053af5
komed3 <pk97@outlook.de>
2025-10-24 09:39:11 +0200

    add LiteralUnion type

--- types/primitives.d.ts
+++ types/primitives.d.ts
@@ -13,10 +13,16 @@ export type Primitive = string | number | boolean | symbol | null | undefined;
 /** Non-primitive */
 export type NonPrimitive = object | Function;
 
+/**
+ * Create a union type that can be assigned a literal or its base type
+ * while maintaining autocomplete for the literal type
+ */
+export type LiteralUnion< T extends U, U = string > = T | ( U & Record< never, never > );
+
 /** Is a type a literal (approximate): string literal, number literal, boolean literal */
 export type IsLiteral< T > = IsAny< T > extends true ? false : (
     string extends T ? false : number extends T ? false : boolean extends T ? false : true
 );
 
 /** Convert a union of primitive literals to a union of corresponding boxed types */
-export type Box< T > = T extends string ? String : T extends number ? Number : T extends boolean ? Boolean : T;
+export type Box<T> = T extends string ? String : T extends number ? Number : T extends boolean ? Boolean : T;
